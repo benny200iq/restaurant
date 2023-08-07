4c06f852c8b342260bfc9a888243db06326c86b0
benny200iq <thanaphat.tan@ku.th>
2023-08-07 13:04:34 +0700

    first

---
+++ restaurant/src/main/java/ku/cs/restaurant/controller/HomeController.java
@@ -0,0 +1,15 @@
+package ku.cs.restaurant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String getAllRestaurants() {
+        return "Yo";
+    }
+}
+
